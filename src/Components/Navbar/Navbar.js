eaa61d5a8092cb6916ad2283f620a0a375feafdb
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-06-07 16:22:53 +0530

    Footer and navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -151,14 +151,14 @@ function Navbar() {
                 <NavLink to="/Portal" activeClassName='active-link' exact><li><a className="">{tab7}</a></li></NavLink>
 
                 <li className="dropdown countries"><a href="#" className='toggle-dropdown'>
-                  <span className='toggle-dropdown'>{tab8}&nbsp;<img src={tld} alt=" Flag" className='flag flagNavbar' /> </span> <i className="bi bi-chevron-down toggle-dropdown arrow " ></i></a>
+                  <span className='toggle-dropdown'>{tab8}&nbsp;<img src={cam} alt=" Flag" className='flag flagNavbar' /> </span> <i className="bi bi-chevron-down toggle-dropdown arrow " ></i></a>
                   <ul className='container1'>
                     <div class="scrollable-menu flagName">
                       
                       <li><a href="#"><img src={aus} alt=" Flag" className='flag' /> Australia</a></li>
                       <li><a href="#"><img src={ban} alt=" Flag" className='flag' /> Bangladesh</a></li>
                       <li><a href="#"><img src={bru} alt=" Flag" className='flag' />Brunei</a></li>
-                      <li><a href="#"><img src={cam} alt=" Flag" className='flag' />Cambodia</a></li>                  
+                      {/* <li><a href="#"><img src={cam} alt=" Flag" className='flag' />Cambodia</a></li>                   */}
                       <li><a href="#"><img src={ind} alt=" Flag" className='flag' />India</a></li>
                       <li><a href="#"><img src={laos} alt=" Flag" className='flag' />Laos</a></li>
                       <li><a href="#"><img src={mld} alt=" Flag" className='flag' />Maldives</a></li>
@@ -167,7 +167,7 @@ function Navbar() {
                       <li><a href="#"><img src={sin} alt=" Flag" className='flag' /> Singapore</a></li>
                       <li><a href="#"><img src={sl} alt=" Flag" className='flag' />Sri Lanka</a></li>
                       <li><a href="#"><img src={tnz} alt=" Flag" className='flag' />Tanzania</a></li>
-                      {/* <li><a href="#"><img src={tld} alt=" Flag" className='flag' />Thailand</a></li> */}
+                      <li><a href="#"><img src={tld} alt=" Flag" className='flag' />Thailand</a></li>
                       <li><a href="#"><img src={wor} alt=" Flag" className='flag' />UAE</a></li>
                     </div>
                   </ul>
